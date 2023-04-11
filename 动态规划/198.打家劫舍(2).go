78d65bf4b43c8e4ee8ad520a69f7039ccf0d92f6
okurusT <okurust@okurusTdeMacBook-Air.local>
2023-04-11 23:59:17 +0800

    category

---
+++ 动态规划/198.打家劫舍(2).go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// 回溯+存储
+func rob(nums []int) int {
+
+	f0, f1 := 0, 0 // 滚动
+	// f0 -> 前两个
+	// f1 -> 前一个
+
+	for _, num := range nums {
+		new_f := max(f1, f0+num)
+		f0 = f1
+		f1 = new_f
+	}
+
+	return f1
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(rob([]int{2, 7, 9, 3, 1}))
+}
