0acb45d0b87e4c8745754af11883eb75b836386e
okurusTZ <okurusT.z@gmail.com>
2023-04-22 13:35:00 +0800

    update

--- prefix_sum/304. 二维区域和检索 - 矩阵不可变.go
+++ prefix_sum/304. 二维区域和检索 - 矩阵不可变.go
@@ -6,19 +6,31 @@ type NumMatrix struct {
 
 func Constructor(matrix [][]int) NumMatrix {
 	preSum := make([][]int, len(matrix))
-	for i := 1; i < len(matrix)+1; i++ {
-		preSum[i] = make([]int, len(matrix[0])+1)
-		for j := 1; j < len(matrix[0])+1; j++ {
-			preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] + matrix[i-1][j-1] - preSum[i-1][j-1]
+	for i, row := range matrix {
+		preSum[i] = make([]int, len(row))
+		for j := range row {
+			if j == 0 {
+				preSum[i][j] = row[j]
+			} else {
+				preSum[i][j] += row[j] + preSum[i][j-1]
+			}
 		}
 	}
+
 	return NumMatrix{
 		PreSum: preSum,
 	}
 }
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
-	return this.PreSum[row2+1][col2+1] - this.PreSum[row1][col1+1] - this.PreSum[row2+1][col1] + this.PreSum[row1][col1]
+	ans := 0
+	for i := row1; i <= row2; i++ {
+		ans += this.PreSum[i][col2] - this.PreSum[i][col1] + this.PreSum[i][col1]
+		if col1 != 0 {
+			ans -= this.PreSum[i][col1-1]
+		}
+	}
+	return ans
 }
 
 /**
