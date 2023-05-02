2cf1103fde9ff7fa8c05a3ca0add29b72d6cfebb
okurusTZ <okurusT.z@gmail.com>
2023-05-03 00:36:50 +0800

    changelog

--- prefix_sum/238. 除自身以外数组的乘积.go
+++ prefix_sum/238. 除自身以外数组的乘积.go
@@ -11,16 +11,10 @@ func productExceptSelf(nums []int) []int {
 	for i := range nums {
 		if i == 0 {
 			left[i] = 1
+			right[len(nums)-1-i] = 1
 		} else {
 			left[i] = left[i-1] * nums[i-1]
-		}
-	}
-
-	for i := len(nums) - 1; i >= 0; i-- {
-		if i == len(nums)-1 {
-			right[i] = 1
-		} else {
-			right[i] = right[i+1] * nums[i+1]
+			right[len(nums)-1-i] = right[len(nums)-i] * nums[len(nums)-i]
 		}
 	}
 
